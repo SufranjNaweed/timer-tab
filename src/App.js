e6129ebbd2eb66630d00e04680f57a35f17fa9e0
SufranjNaweed <sufranj.naweed@gmail.com>
2020-03-31 05:33:52 +0200

    add : timer form

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Timer from './Components/Timer';
+import Task from './Components/Tasks';
 
 function App() {
   return (
     <div className="App">
       <Timer />
+      <Task />
     </div>
   );
 }
--- src/Components/Timer/index.js
+++ src/Components/Timer/index.js
@@ -1,13 +1,15 @@
 import React, {useState} from 'react';
 import useInterval from '../../CustomsHooks/useInterval';
 
-
 const Timer = () => {
 
     const [delay, setDelay] = useState(1000);
     const [minutes, setMinutes] = useState(0);
-    const [seconds, setSeconds] = useState(12);
-    const [isRunning, setIsRunning] = useState(true)
+    const [seconds, setSeconds] = useState(25);
+    const [isRunning, setIsRunning] = useState(false);
+
+    const [inputMinutes, setInputMinutes] = useState('');
+    const [inputSeconds, setInputSeconds] = useState('');
 
     useInterval(() => {
         // Format timer title
@@ -27,18 +29,27 @@ const Timer = () => {
                 setSeconds(59);
             }
         }
-
     },
         // handle timer delay
         isRunning ? delay : null
     );
 
+    const handleSubmitTimer = (e) => {
+        e.preventDefault();
+        setMinutes(inputMinutes);
+        setSeconds(inputSeconds);
+        setIsRunning(true);
+    }
+
     return (
         <div>
             <h1>Time Remaining : {minutes}:{seconds >= 10 ? seconds : "0" + seconds}</h1>
-            <div>
-                
-            </div>
+            
+            <form onSubmit={handleSubmitTimer}>
+                <input placeholder="minutes"  value={inputMinutes} onChange={(e) => setInputMinutes(e.target.value)}/>
+                <input placeholder="seconds" value={inputSeconds} onChange={(e) => setInputSeconds(e.target.value)}/>
+                <input type="submit" value="START" />
+            </form>
         </div>
     );
 }
