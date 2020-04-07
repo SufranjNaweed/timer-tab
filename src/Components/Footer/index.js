913b0a518ef985936b144610719e60a02c10d6ce
SufranjNaweed <sufranj.naweed@gmail.com>
2020-04-07 05:30:13 +0200

    update : footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>Designed &amp; Developped by Naweed SUFRANJ</p>
-            <p>source code here :  <a href="https://github.com/SufranjNaweed/">https://github.com/SufranjNaweed/</a></p>
+            <p>Designed &amp; Developped by <a href="https://naweed-sufranj.fr/" target="_blank">Naweed SUFRANJ</a></p>
+            <p>source code here :  <a href="https://github.com/SufranjNaweed/timer-task" target="_blank">https://github.com/SufranjNaweed/timer-task</a></p>
         </footer>
     );
 }
